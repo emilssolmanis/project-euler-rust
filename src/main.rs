50851fcc267c0952c66c1340808316febc1c292c
Emils Solmanis <emils.solmanis@uswitch.com>
2018-09-20 11:53:12 +0100

    problem 3

--- src/main.rs
+++ src/main.rs
@@ -5,4 +5,5 @@ use rust_fuckery::solutions;
 fn main() {
     println!("problem 1: {}", solutions::problem_1::solve(1000));
     println!("problem 2: {}", solutions::problem_2::solve(4_000_000));
+    println!("problem 3: {}", solutions::problem_3::solve(600851475143));
 }
--- src/solutions/problem_3.rs
+++ src/solutions/problem_3.rs
@@ -1,23 +1,25 @@
 struct Primes {
-    previous: Vec<u32>
+    previous: Vec<u64>
 }
 
 impl Primes {
     pub fn new() -> Primes {
-        Primes { previous: vec }
+        Primes { previous: vec![2] }
     }
 
-    fn is_prime(&self, i: u32) -> bool {
-        let test_up_to: u32 = (i as f64).sqrt().ceil() as u32;
+    fn is_prime(&self, i: u64) -> bool {
+        let test_up_to: u64 = (i as f64).sqrt().ceil() as u64;
+
         for prime in self.previous.iter().take_while(|i| **i <= test_up_to) {
             if i % *prime == 0 {
                 return false;
-            }
-        }
+            };
+        };
+
         true
     }
 
-    fn compute_new_prime(&mut self, prev_prime: u32) {
+    fn compute_new_prime(&mut self, prev_prime: u64) {
         for number_under_test in (prev_prime + 1).. {
             if self.is_prime(number_under_test) {
                 self.previous.push(number_under_test);
@@ -28,32 +30,40 @@ impl Primes {
 }
 
 impl Iterator for Primes {
-    type Item = u32;
+    type Item = u64;
 
     fn next(&mut self) -> Option<<Self as Iterator>::Item> {
-        let prev_prime = self.previous.last().or(Some(&2)).unwrap();
+        let mut default = 2;
+        let prev_prime = *self.previous.last_mut().unwrap_or(&mut default);
 
-        self.compute_new_prime(*prev_prime);
+        self.compute_new_prime(prev_prime);
 
-        Some(*prev_prime)
+        Some(prev_prime)
     }
 }
 
-pub fn solve() {
-
+pub fn solve(num: u64) -> u64 {
+    Primes::new()
+        .take_while(|p| *p <= (num as f64).sqrt().ceil() as u64)
+        .filter(|p| num % p == 0)
+        .last()
+        .unwrap()
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-//    #[test]
-//    fn test_it() {
-//        let primes: Vec<u32> = Primes::new().take(5).collect();
-//        assert_eq!(primes, vec![2, 3, 5, 7, 11]);
-//    }
     #[test]
-    fn test_more_it() {
-        Primes::new();
+    fn test_it() {
+        let primes: Vec<u64> = Primes::new().take(7).collect();
+        assert_eq!(primes, vec![2, 3, 5, 7, 11, 13, 17]);
+    }
+
+    #[test]
+    fn test_example() {
+        let num = 13195;
+
+        assert_eq!(29, solve(num));
     }
 }
